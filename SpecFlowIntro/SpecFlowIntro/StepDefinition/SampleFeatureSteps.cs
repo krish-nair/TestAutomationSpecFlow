e66917d69efc445fafdb29e802607de67b0002be
krish-nair <krish.nair101@gmail.com>
2018-11-24 21:22:42 +1300

    run the smokeTest

---
+++ SpecFlowIntro/SpecFlowIntro/StepDefinition/SampleFeatureSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowIntro
+{
+    [Binding]
+    class SampleFeatureSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int numbers)
+        {
+            Console.WriteLine(numbers);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("Pressed the Add button");
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result == 120)
+                Console.WriteLine("Test PASSED");
+            Console.WriteLine("Test FAILED");
+        }
+
+    }
+}
